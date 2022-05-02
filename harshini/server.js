b3d9bb9d847d757c37daaaa74eb46e065f94f728
Harshini Komali <harshinikomali73@gmail.com>
2022-05-01 21:20:27 -0700

    Add server.js and package.json

---
+++ harshini/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const dbConfig = require("./config/db.config");
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+const db = require("./models");
+
+
+db.mongoose
+  .connect(`mongodb://${dbConfig.DB_HOST}:${dbConfig.DB_PORT}/${dbConfig.DB_DATABSE}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+  function initial() {
+  }
+  
+//routes
+const route = require("./routes/user.routes");
+app.use(route);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT,() => {
+	console.log(`Server is running on port ${PORT}`);
+});
