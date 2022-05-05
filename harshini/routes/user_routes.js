bb4756aea578108f18ba16dad2af14dab542c5ae
Harshini Komali <harshinikomali73@gmail.com>
2022-05-05 16:01:41 -0700

    Add files via upload

--- harshini/routes/user_routes.js
+++ harshini/routes/user_routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const controller = require("../controllers/user.controller");
+const controller = require("../controllers/user_controller");
 const router = express();
 router.post('/signin',controller.signin);
-router.post('/signup',controller.signup);
+router.post('/signup',controller.signUp);
+router.put('/updatedetails',controller.updateProfile);
 module.exports = router;
